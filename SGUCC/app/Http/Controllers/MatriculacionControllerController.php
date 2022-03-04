ecc9080e762d3673ad48479edf61c08cc7435069
Mariano <benitezmariano5@gmail.com>
2022-03-04 08:19:18 -0300

    mat1

---
+++ SGUCC/app/Http/Controllers/MatriculacionControllerController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\dev\MatriculacionController;
+use Illuminate\Http\Request;
+
+class MatriculacionControllerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\dev\MatriculacionController  $matriculacionController
+     * @return \Illuminate\Http\Response
+     */
+    public function show(MatriculacionController $matriculacionController)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\dev\MatriculacionController  $matriculacionController
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(MatriculacionController $matriculacionController)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\dev\MatriculacionController  $matriculacionController
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, MatriculacionController $matriculacionController)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\dev\MatriculacionController  $matriculacionController
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(MatriculacionController $matriculacionController)
+    {
+        //
+    }
+}
